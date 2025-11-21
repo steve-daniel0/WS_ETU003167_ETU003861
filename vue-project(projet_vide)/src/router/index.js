52407a6e103716a804f550dc9395716f49b18c7a
steve-daniel0 <stevens.rakotomalala@gmail.com>
2025-11-21 12:14:47 +0300

    details par moyenne

--- vue-project(projet_vide)/src/router/index.js
+++ vue-project(projet_vide)/src/router/index.js
@@ -4,12 +4,14 @@ import ListeStudentsBySemester from '../components/ListeStudentsBySemester.vue'
 import ListeEtu from '../components/ListeEtu.vue'
 import DetailEtu from '../components/DetailEtu.vue'
 import GradesByYear from '../components/GradesByYear.vue'
+import NoteSemester from '@/components/NoteSemester.vue'
 
 const routes = [
   // { path: '/', name: 'Semesters', component: ListeSemestre },
   { path: '/semester/:id/students', name: 'SemesterStudents', component: ListeStudentsBySemester, props: true },
   { path: '/etudiant/:id', name: 'DetailEtu', component: DetailEtu, props: true },
   { path: '/grade/L/:year/:id_student', name: 'GradesByYear', component: GradesByYear, props: true },
+  { path: '/notes/semester/:id_semester/student/:id_student', name: 'NoteSemester', component: NoteSemester, props: true },
   { path: '/', name: 'ListeEtu', component: ListeEtu },
 ]
 
